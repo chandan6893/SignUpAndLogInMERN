1ab2fd8cf2bd55d9a965da62259a73a3d986c879
Chandan Kumar <krc6893@gmail.com>
2023-11-26 10:56:20 +0530

    g nodemon

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const EmployeeModel = require("./models/Employee");
 
 const UserModel = require("./models/User");
-
+URL = "mongodb://127.0.0.1:27017/employee";
 mongoose.connect("mongodb://127.0.0.1:27017/employee");
 
 app.post("/register", (req, res) => {
