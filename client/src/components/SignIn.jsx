fdde9a5045ae80764c79bfca30398a4650e60438
Chandan Kumar <krc6893@gmail.com>
2023-11-26 11:29:00 +0530

    https://signupandloginapp.onrender.com,navigate

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -105,9 +105,7 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              {/* <Link href="/register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link> */}
+             
               <Button onClick={() => navigate("/register")}>
                 Don't have an account? Sign Up
               </Button>
